d8a5691cea897e2cd1fc03c410fc6911cfdeced6
Marcelo Jorge Vieira <metal@alucinados.com>
2020-09-07 22:53:18 -0300

    Frontend: Formatting all JS files
    Added Prettier (Opinionated Code Formatter)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,31 @@
-// in src/App.js
 import * as React from "react";
-import { Admin, Resource, ListGuesser, fetchUtils } from 'react-admin';
-import jsonServerProvider from 'ra-data-json-server';
+import { Admin, Resource, ListGuesser, fetchUtils } from "react-admin";
+import jsonServerProvider from "ra-data-json-server";
 
-import { BrokerList, BrokerEdit, BrokerCreate, BrokerIcon } from './Brokers';
-import { StockOperationList, StockOperationEdit, StockOperationCreate, StockOperationIcon } from './StockOperations';
+import { BrokerList, BrokerEdit, BrokerCreate, BrokerIcon } from "./Brokers";
+import {
+  StockOperationCreate,
+  StockOperationEdit,
+  StockOperationIcon,
+  StockOperationList,
+} from "./StockOperations";
 
-const dataProvider = jsonServerProvider('http://localhost:8000');
+const dataProvider = jsonServerProvider("http://localhost:8000");
 const App = () => (
-    <Admin dataProvider={dataProvider}>
-         <Resource name="brokers" list={BrokerList} edit={BrokerEdit} create={BrokerCreate}/>
-         <Resource name="stocks/operations" list={StockOperationList} edit={StockOperationEdit} create={StockOperationCreate}/>
-    </Admin>
+  <Admin dataProvider={dataProvider}>
+    <Resource
+      name="brokers"
+      list={BrokerList}
+      edit={BrokerEdit}
+      create={BrokerCreate}
+    />
+    <Resource
+      name="stocks/operations"
+      list={StockOperationList}
+      edit={StockOperationEdit}
+      create={StockOperationCreate}
+    />
+  </Admin>
 );
 
 export default App;
