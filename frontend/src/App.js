cd449eb5342d8b62e21badce2835fe2783bb3172
Marcelo Jorge Vieira <metal@alucinados.com>
2020-10-02 14:07:03 -0300

    Added custom menu component
    If you want to add or remove menu items, for instance to link to
    non-resources pages, you can create your own menu component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,16 @@
 import * as React from "react";
 import { Admin, Resource } from "react-admin";
 import jsonServerProvider from "ra-data-json-server";
+import {
+  AttachMoney as AttachMoneyIcon,
+  FolderSpecial as FolderSpecialIcon,
+  ShoppingCart as ShoppingCartIcon,
+  Store as StoreIcon,
+} from "@material-ui/icons";
 
 import { BrokerList, BrokerEdit, BrokerCreate } from "./Brokers";
 import { EventCreate, EventEdit, EventList } from "./Events";
+import Menu from "./Menu";
 import {
   PortfolioShow,
   PortfolioList,
@@ -14,18 +21,22 @@ import { PositionList } from "./Positions";
 
 const dataProvider = jsonServerProvider("http://localhost:8000/api/v1");
 const App = () => (
-  <Admin dataProvider={dataProvider}>
+  <Admin dataProvider={dataProvider} menu={Menu}>
     <Resource
       name="brokers"
       list={BrokerList}
       edit={BrokerEdit}
       create={BrokerCreate}
+      icon={StoreIcon}
+      options={{ label: "Brokers" }}
     />
     <Resource
       name="events"
       list={EventList}
       edit={EventEdit}
       create={EventCreate}
+      icon={ShoppingCartIcon}
+      options={{ label: "Events" }}
     />
     <Resource
       name="portfolios"
@@ -33,9 +44,16 @@ const App = () => (
       list={PortfolioList}
       edit={PortfolioEdit}
       create={PortfolioCreate}
+      icon={FolderSpecialIcon}
+      options={{ label: "Portfolios" }}
     />
-    <Resource name="portfolios/positions" />
-    <Resource name="positions" list={PositionList} />
+    <Resource name="portfolios/positions" icon={StoreIcon} />
+    <Resource
+      name="positions"
+      list={PositionList}
+      icon={AttachMoneyIcon}
+      options={{ label: "Positions" }}
+    />
   </Admin>
 );
 
