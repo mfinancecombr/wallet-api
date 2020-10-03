11d493c726ba9ca4d2cd29da39967d5455b89c3e
Marcelo Jorge Vieira <metal@alucinados.com>
2020-10-03 00:02:04 -0300

    Portfolio: Added allocation chart

---
+++ frontend/src/chartColors.js
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2020, Marcelo Jorge Vieira (https://github.com/mfinancecombr)
+ *
+ * License: BSD 3-Clause
+ */
+
+export const chartColors = [
+  "#0088FE",
+  "#5DADE2",
+  "#00C49F",
+  "#FFBB28",
+  "#FF8042",
+  "#003F5C",
+  "#665191",
+  "#A05195",
+  "#D45087",
+  "#F95D6A",
+  "#2F4B7C",
+  "#FF7C43",
+  "#379469",
+  "#DE425B",
+  "#58A066",
+  "#EC838A",
+  "#78AB63",
+  "#F3BABC",
+  "#C5E1A5",
+  "#7986CB",
+];
